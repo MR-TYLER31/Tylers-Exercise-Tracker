27ca0f3562f176a38866e54db47695b8177c20a0
Tyler Snyder <tyler@Tylers-MacBook-Pro.local>
2020-03-23 15:02:18 -0600

    added new workouts and exercises

--- controller/api-routes.js
+++ controller/api-routes.js
@@ -15,62 +15,31 @@ router.get("/all", function(req, res) {
     });
 });
 
-// router.get("/exercises", (req, res) => {
-//   db.Exercise.find({})
-//     .then(dbExercise => {
-//       res.json(dbExercise);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
+router.post("/", ({ body }, res) => {
+  db.Workout.create(body)
+    .then(dbUser => {
+      res.json(dbUser);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
-// router.get("/workouts", (req, res) => {
-//   db.Workout.find({})
-//     .then(dbWorkout => {
-//       res.json(dbWorkout);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
-
-// router.post("/submit-workout", ({ body }, res) => {
-//   db.Workout.create(body)
-//     .then(dbUser => {
-//       res.json(dbUser);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
-
-// router.post("/submit-exercise", ({ body }, res) => {
-//   db.Exercise.create(body)
-//     .then(({ _id }) =>
-//       db.Workout.findOneAndUpdate(
-//         {},
-//         { $push: { exercises: _id } },
-//         { new: true }
-//       )
-//     )
-//     .then(dbUser => {
-//       res.json(dbUser);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
-
-// router.get("/populatedexercises", (req, res) => {
-//   db.Workout.find({})
-//     .populate("exercises")
-//     .then(dbUser => {
-//       res.json(dbUser);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
+router.post("/submit", ({ body }, res) => {
+  db.Exercise.create(body)
+    .then(({ _id }) =>
+      db.Workout.findOneAndUpdate(
+        {},
+        { $push: { exercises: _id } },
+        { new: true }
+      )
+    )
+    .then(dbUser => {
+      res.json(dbUser);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 module.exports = router;
--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,126 @@
-function getResults() {
+function getToday() {
   // Empty any results currently on the page
   $("#new-workout").empty();
   // Grab all of the current notes
   $.get("/all", function(data) {
     // For each note...
-    console.log(data);
+    // console.log(data);
     for (var i = 0; i < data.length; i++) {
-      console.log(data[i]);
-      // ...populate #results with a p-tag that includes the note's title and object id
-      $("#new-workout").prepend(`
-      <div class="col-md-12 text-center">
-    
-        <p>${data[i].date}</p>
-      <h2>Work out: ${data[i].name}</h2>
-      <h3>Exercises: ${data[i].exercises[i].exerciseName}</h3>
-      <p>Sets: ${data[i].exercises[i].setNum}</p>
-      <p>Reps: ${data[i].exercises[i].repNum}</p>
-      <p>Weight: ${data[i].exercises[i].weight}</p>
-      
-      </div>`);
+      let colDiv = $("<div>");
+      colDiv.addClass("col-md-12 text-center");
+      $("#new-workout").append(colDiv);
+
+      let h1 = $("<h1>");
+      h1.html("Todays Workout");
+      $(colDiv).append(h1);
+
+      let h2 = $("<h2>");
+      h2.addClass("workout-name");
+      h2.html(`${data[i].name}`);
+      h1.append(h2);
+
+      let item = data[i].exercises;
+      for (var j = 0; j < item.length; j++) {
+        console.log(item[j]);
+
+        let h3 = $("<h3>");
+        h3.html(`Exercise: ${item[j].exerciseName}`);
+        $(colDiv).append(h3);
+
+        let set = $("<p>");
+        set.html(`Sets: ${item[j].setNum}`);
+        $(colDiv).append(set);
+
+        let rep = $("<p>");
+        rep.html(`Reps: ${item[j].repNum}`);
+        $(colDiv).append(rep);
+
+        let weight = $("<p>");
+        weight.html(`Weight: ${item[j].weight}`);
+        $(colDiv).append(weight);
+      }
+    }
+
+    $("#add-new").append(`
+     <div class="col-md-12">
+                    <button class="btn btn-success btn-block my-5" data-toggle="collapse" href="#collapseExample"
+                        role="button" aria-expanded="false" aria-controls="collapseExample">add new exercise</button>
+                    <div class="collapse" id="collapseExample">
+                        <div class="card">
+                            <div class="card-body">
+                                <form action="/submit" method="POST" id="workout-form">
+                                    <div class="form-group">
+                                        <input type="text" name="exerciseName" class="form-control" id="exercise-name"
+                                            placeholder="Enter Exercise">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="text" name="setNum" class="form-control" id="set-number"
+                                            placeholder="Enter Sets">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="text" name="repNum" class="form-control" id="rep-number"
+                                            placeholder="Enter Reps">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="text" name="weight" class="form-control" id="weight"
+                                            placeholder="Enter Weight in Pounds">
+                                    </div>
+                                    <button type="submit" class="btn btn-primary btn-block" id="add-workout">Add
+                                    </button>
+                                </form>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+    `);
+  });
+}
+
+function getPrevious() {
+  // Empty any results currently on the page
+  $("#prev-workout").empty();
+  // Grab all of the current notes
+  $.get("/all", function(data) {
+    // For each note...
+    console.log(data);
+    for (var i = 1; i < data.length; i++) {
+      let colDiv = $("<div>");
+      colDiv.addClass("col-md-12 text-center");
+      $("#prev-workout").append(colDiv);
+
+      let h1 = $("<h1>");
+      h1.html("Yesterdays Workout");
+      $(colDiv).append(h1);
+
+      let h2 = $("<h2>");
+      h2.addClass("workout-name");
+      h2.html(`${data[i].name}`);
+      h1.append(h2);
+
+      let item = data[i].exercises;
+      for (var j = 0; j < item.length; j++) {
+        console.log(item[j]);
+
+        let h3 = $("<h3>");
+        h3.html(`Exercise: ${item[j].exerciseName}`);
+        $(colDiv).append(h3);
+
+        let set = $("<p>");
+        set.html(`Sets: ${item[j].setNum}`);
+        $(colDiv).append(set);
+
+        let rep = $("<p>");
+        rep.html(`Reps: ${item[j].repNum}`);
+        $(colDiv).append(rep);
+
+        let weight = $("<p>");
+        weight.html(`Weight: ${item[j].weight}`);
+        $(colDiv).append(weight);
+      }
     }
   });
 }
 
-getResults();
+getToday();
+getPrevious();
